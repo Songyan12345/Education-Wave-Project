52cde3a12f7415479b68eabfa36e1472981a342d
Songyan12345 <64071081+Songyan12345@users.noreply.github.com>
2020-10-19 11:37:28 +1300

    V4, added top tutor slideshow

--- features.js
+++ features.js
@@ -11,8 +11,20 @@ function changeDisplay(name) {
             document.getElementById(pages[i]).style.display = "none";
         }
         current = name;
-        window.scrollTo(0, 0);
     }
+
+    // Reset tutor filter
+    filterSelection("all");
+    let container = document.getElementById("filterButtons");
+    let buttons = container.getElementsByClassName("filterBtn");
+    for (let i = 0; i < buttons.length; i++) {
+        let current = document.getElementsByClassName("filterBtn active");
+        current[0].className = current[0].className.replace(" active", "");
+        buttons[0].className += " active";
+    }
+
+    // Scroll to top of page
+    window.scrollTo(0, 0);
 }
 
 // Filter content
@@ -25,6 +37,17 @@ function filterSelection(c) {
         if (elements[i].className.indexOf(c) != -1) {
             showElement(elements[i]);
         };
+    }
+
+    // Highlight selected group
+    let container = document.getElementById("filterButtons");
+    let buttons = container.getElementsByClassName("filterBtn");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            let current = document.getElementsByClassName("filterBtn active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
     }
 }
 
@@ -40,17 +63,6 @@ function hideElement(element) {
         name = name.substring(0, name.length - 4);
     }
     element.className = name;
-}
-
-// Highlight selected group
-let container = document.getElementById("filterButtons");
-let buttons = container.getElementsByClassName("filterBtn");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-        let current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
 }
 
 function showDetails(name) {
@@ -98,5 +110,24 @@ function showDetails(name) {
     }
 }
 
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slideshowSlides");
+    let info = document.getElementsByClassName("tutorInfo");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < slides.length; i++) {
+        info[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n - 1].style.display = "block";
+    info[n - 1].style.display = "block";
+    dots[n - 1].className += " active";
+}
+
 changeDisplay("index");
 filterSelection("all");
+showSlides(1);
