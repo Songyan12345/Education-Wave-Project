e7a295162de7155a0339f92d07591df8255d3b7d
Songyan12345 <64071081+Songyan12345@users.noreply.github.com>
2020-10-19 10:37:28 +1300

    V2, added tutor filtering functionality

--- features.js
+++ features.js
@@ -15,4 +15,43 @@ function changeDisplay(name) {
     }
 }
 
+// Filter content
+function filterSelection(c) {
+    let elements = document.getElementsByClassName("column");
+    if (c == "all") c = "";
+
+    for (let i = 0; i < elements.length; i++) {
+        hideElement(elements[i]);
+        if (elements[i].className.indexOf(c) != -1) {
+            showElement(elements[i]);
+        };
+    }
+}
+
+// Show elements
+function showElement(element) {
+    element.className += " show";
+}
+
+// Hide elements
+function hideElement(element) {
+    let name = element.className;
+    if (name.indexOf("show") != -1) {
+        name = name.substring(0, name.length - 4);
+    }
+    element.className = name;
+}
+
+// Highlight selected group
+let container = document.getElementById("filterButtons");
+let buttons = container.getElementsByClassName("filterBtn");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
 changeDisplay("index");
+filterSelection("all");
